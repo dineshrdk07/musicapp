35f6403b0a89fa84b40e745acba7d448dacc072e
Ajitha-P <ajithaprave@gmail.com>
2023-12-12 07:27:52 -0800

    Added Episode objects

---
+++ src/main/java/com/cts/musicapp/model/Episodes.java
@@ -0,0 +1,12 @@
+package com.cts.musicapp.model;
+
+public class Episodes {
+    private String href;
+    private Integer limit;
+    private String next;
+    private Integer offset;
+    private String previous;
+    private Integer total;
+    private Episode episode;
+
+}
---
+++ src/main/java/com/cts/musicapp/model/ResumePoint.java
@@ -0,0 +1,6 @@
+package com.cts.musicapp.model;
+
+public class ResumePoint {
+    private Boolean fully_played;
+    private Integer resume_position_ms;
+}
